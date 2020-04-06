42444f4a4b6ce484a029b0b1d93c1c76599394c7
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2020-04-05 21:59:19 -0400

    Fixed logging procedures for test scripts. Added more detailed output in subfolders.

--- test/daily.sh
+++ test/daily.sh
@@ -12,14 +12,12 @@ cd "$scriptDir"
 dir=$1
 transDir=$dir/trans
 TESTS=$dir/tests/*
-BUFOUT=$dir/out.txt
 
 # Test counter
 testCnt=1
 
 # Formatting
 green="\e[0;49;32m"
-#red="\e[0;49;31m"
 reset="\e[0m"
 
 # Remove old transaction files
@@ -29,8 +27,6 @@ rm $dir/out/daily_transaction_file.txt
 # For frontend
 touch $dir/in/current_user_accounts_file.txt
 touch $dir/in/available_items_file.txt
-# Temp buffer
-touch $BUFOUT
 
 # Copy in files into out files
 cat "${dir}/in/current_user_accounts_file.txt" >"${dir}/out/current_user_accounts_file.txt"
@@ -40,19 +36,22 @@ cat "${dir}/in/available_items_file.txt" >"${dir}/out/available_items_file.txt"
 for f in $TESTS
 do
 	touch $transDir/daily_transaction_file_$testCnt.txt
-	echo "Run ${testCnt}: ${f}"
-	../SQA-Front-End/auction-system.exe "${dir}/in/current_user_accounts_file.txt" "${dir}/in/available_items_file.txt" "${transDir}/daily_transaction_file_${testCnt}.txt" <$f #>$BUFOUT
+	touch $dir/termout/termout_$testCnt.txt
+	echo "Running test ${testCnt}"
+	../SQA-Front-End/auction-system.exe "${dir}/in/current_user_accounts_file.txt" "${dir}/in/available_items_file.txt" "${transDir}/daily_transaction_file_${testCnt}.txt" <$f >"${dir}/termout/termout_${testCnt}.txt"
 
 	((testCnt++))
 done
 
-rm $BUFOUT
-
 # Merge transaction files
 cat $transDir/* >"${dir}/out/daily_transaction_file.txt"
 
-echo -e "\n${green}Created merged transaction file.${reset}\n"
+echo -e "Running backend"
+
+touch ${dir}/termout/termout_backend.txt
 
 # Run backend
 cd $dir/out
-java -jar ../../../out/artifacts/SQA_Back_End_jar/SQA-Back-End.jar
+java -jar ../../../out/artifacts/SQA_Back_End_jar/SQA-Back-End.jar > "../termout/termout_backend.txt"
+echo -e "${green}Output files created.${reset}"
+echo "Output files are found in the out and termout folders."
--- test/weekly.sh
+++ test/weekly.sh
@@ -1,13 +1,22 @@
+# Formatting
+green="\e[0;49;32m"
+reset="\e[0m"
+
 for day in {1..5}
 do
+	echo "Testing day ${day}"
 	bash daily.sh "day${day}" >/dev/null
 	if [ $day -ne 5 ]
 	then
 		nextDay=$(($day+1))
 		cp --preserve "day${day}/out/available_items_file.txt" "day${nextDay}/in/available_items_file.txt"
 		cp --preserve "day${day}/out/current_user_accounts_file.txt" "day${nextDay}/in/current_user_accounts_file.txt"
-		#cat "day${day}/out/available_items_file.txt" >"day${nextDay}/in/available_items_file.txt"
-		#cat "day${day}/out/current_user_accounts_file.txt" >"day${nextDay}/in/current_user_accounts_file.txt"
 	fi
 done
 
+cp --preserve "day${nextDay}/in/available_items_file.txt" "weeklyOut/available_items_file.txt"
+cp --preserve "day${nextDay}/in/current_user_accounts_file.txt" "weeklyOut/current_user_accounts_file.txt"
+
+echo -e "${green}Output files created.${reset}"
+echo "Weekly logs found in weeklyOut folder."
+echo "Daily logs found in each day's folder under out or termout."
